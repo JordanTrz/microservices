36bea35a808753df16d57b258163677345fe93a2
Jordan <jordan.terrazos@gmail.com>
2022-03-08 21:30:37 -0500

    d01

---
+++ categorias/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const {config} = require('../config');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(require('./categoria'));
+
+app.listen(config.categorias.port,function(){
+    console.log(`ms categorias : http://localhost:${config.categorias.port}`);
+})
